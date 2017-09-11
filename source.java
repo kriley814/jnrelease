ddd9542fc4fd1007b1ba36816f273472b97862ab
Chris Riley <chris.riley@cprime.com>
2017-09-11 08:49:54 -0400

    Revert "US-5 created addition method."
    This reverts commit 17d842b5bf2f1eb3bab4b5f4cfd44fa6ed6576b6.
    
    Accidentally pushed change to develop vs. feature/us-5. Undoing commit.

--- source.java
+++ source.java
@@ -7,10 +7,3 @@ public void main(String args[]){
 	Sample implentation code implemented by kriley.
 
 }
-
-//US-5 Added a method to add two numbers
-public int addTwoNumber(int x, int y){
-
-	return (x+y);
-}
-
