eba66b0f8d7582660a67d47aa49d1423476784a6
kriley814 <kim.riley@cprime.com>
2017-10-05 10:56:04 -0400

    US-8 print two numbers kriley

--- source.java
+++ source.java
@@ -35,7 +35,12 @@ public int divNumbers(int x, int y) {
 }
 
 //Divid by zero
-public int divByZero(int x, int y) {
+//public int divByZero(int x, int y) {
 
-	return (x+y)/0;
+//	return (x+y)/0;
+//}
+
+// Print two numbers
+public void printNumbers(int x, int y) {
+	System.out.println("x: " + x + " y: " + y);
 }
